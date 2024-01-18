f2a16d59bb9dc5a3c5ef9e3ef610bdc98f5e09f9
Elton Araujo de Castro <eltonaraujodecastro@gmail.com>
2024-01-18 14:44:42 -0300

    versão funcional

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS nodedb;
 CREATE DATABASE IF NOT EXISTS nodedb;
 USE nodedb;
-CREATE TABLE IF NOT EXISTS PEOPLE (id int NOT NULL auto_increment, nome varchar(250), primary key(id));
+CREATE TABLE IF NOT EXISTS people (id int NOT NULL auto_increment, name varchar(250), primary key(id));
--- node/index.js
+++ node/index.js
@@ -1,25 +1,55 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const config = {
+const express = require('express');
+const mysql = require('mysql');
+var random_name = require('node-random-name');
+const app = express();
+const port = 3000;
+
+// Configure MySQL connection
+const db = mysql.createConnection({
     host: 'mysqlDB',
     user: 'root',
     password: 'root',
-    database:'nodedb'
-};
-const selectPeople = [];
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
+    database: 'nodedb'
+});
 
-const sql = `INSERT INTO PEOPLE(nome) values('Wesley')`
-connection.query(sql)
-connection.end()
+// Connect to MySQL
+db.connect((err) => {
+    if (err) {
+        console.error('MySQL connection error:', err);
+    } else {
+        console.log('Connected to MySQL database');
+    }
+});
 
+// Define a route to fetch and display people names
+app.get('/', (req, res) => {
+    const insertQuery = 'INSERT INTO people (name) VALUES (?)';
 
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle</h1>')
-})
+    firstName = random_name({ first: true });
 
-app.listen(port, ()=> {
-    console.log('Rodando na porta ' + port)
-})
+    db.query(insertQuery, [firstName], (err) => {
+        if (err) {
+            console.error('MySQL insert error:', err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            console.log(`Inserted: ${firstName}`);
+        }
+    });
+
+    const query = 'SELECT * FROM people';
+
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('MySQL query error:', err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            const peopleList = results.map((person) => person.name);
+            res.send('<h1>Full Cycle</h1><br>' + peopleList);
+        }
+    });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
